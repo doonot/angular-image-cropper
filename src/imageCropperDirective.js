44ef538d3756e797b640ec02df1719a3b1986879
Patrick Breiter <patrick.breiter@suva.ch>
2017-08-30 15:46:18 +0200

    Fixes

--- src/imageCropperDirective.js
+++ src/imageCropperDirective.js
@@ -1,54 +1,68 @@
 if (ON_TEST) {
-  require('./imageCropperDirective.test.js')(angular);
+	require('./imageCropperDirective.test.js')(angular);
 }
 
-module.exports = function(angular, Cropper) {
-  require('./angular-image-cropper.scss');
-  angular
-    .module('imageCropper')
-    .directive('imageCropper', function() {
-      return {
-        restrict: 'E',
-        scope: {
-          centerOnInit: '@',
-          checkCrossOrigin: '@',
-          cropCallback: '&',
-          api: '&',
-          fitOnInit: '@',
-          height: '@',
-          imageUrl: '@',
-          showControls: '@',
-          width: '@',
-          zoomStep: '@',
-          actionLabels: '&'
-        },
-        bindToController: true,
-        controllerAs: 'vm',
-        controller: function() {
-          var self = this;
+module.exports = function (angular, Cropper) {
+	require('./angular-image-cropper.scss');
+	angular
+		.module('imageCropper')
+		.directive('imageCropper', function () {
 
-          // Get action labels.
-          this.actionLabels = this.actionLabels();
+			var imageCropperController = function ($scope) {
+				var self = this;
 
-          // Get callback.
-          this.apiCallback = this.api();
-          this.cropCallback = this.cropCallback();
+				// Get action labels.
+				this.actionLabels = this.actionLabels();
 
-          // Eval for boolean values.
-          this.fitOnInit = eval(this.fitOnInit);
-          this.centerOnInit = eval(this.centerOnInit);
-          this.checkCrossOrigin = eval(this.checkCrossOrigin);
-          this.showControls = eval(this.showControls);
+				// Get callback.
+				this.apiCallback = this.api();
+				this.cropCallback = this.cropCallback();
 
-          this.init = function() {
-            this.target = this.element;
-            this.api = new Cropper(self);
-          }
-        },
-        'link': function(scope, element, attributes, controller) {
-          controller.element = element[0];
-          controller.init();
-        }
-      };
-    });
+				// Eval for boolean values.
+				this.fitOnInit = eval(this.fitOnInit);
+				this.centerOnInit = eval(this.centerOnInit);
+				this.checkCrossOrigin = eval(this.checkCrossOrigin);
+				this.showControls = eval(this.showControls);
+
+				this.init = function () {
+					this.target = this.element;
+					this.api = new Cropper(self);
+
+					$scope.$watch('vm.imageUrl', function (newImageUrl, oldImageUrl) {
+						if (angular.isDefined(newImageUrl)
+							&& !angular.equals(newImageUrl, oldImageUrl)
+						) {
+							console.log("change image!!");
+							self.api.changeImage(newImageUrl);
+						}
+					});
+				};
+			};
+
+			imageCropperController.$inject = ['$scope'];
+
+			return {
+				restrict: 'E',
+				scope: {
+					centerOnInit: '@',
+					checkCrossOrigin: '@',
+					cropCallback: '&',
+					api: '&',
+					fitOnInit: '@',
+					height: '@',
+					imageUrl: '@',
+					showControls: '@',
+					width: '@',
+					zoomStep: '@',
+					actionLabels: '&'
+				},
+				bindToController: true,
+				controllerAs: 'vm',
+				controller: imageCropperController,
+				link: function (scope, element, attributes, controller) {
+					controller.element = element[0];
+					controller.init();
+				}
+			};
+		});
 };
